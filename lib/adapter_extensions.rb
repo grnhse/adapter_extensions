55b5ddd607ecfb3cb465e48abd25510563b0ccde
Tom Phillips <tdphillipsjr@gmail.com>
2021-05-11 14:40:08 -0400

    re-order alias

--- lib/adapter_extensions.rb
+++ lib/adapter_extensions.rb
@@ -4,13 +4,14 @@ class ActiveRecord::Base
   class << self
     # for 1.8.7, modules loaded more than once that use alias_method_chain will 
     # cause stack level too deep errors - make sure we avoid this
-    unless self.method_defined?(:establish_connection_without_adapter_extensions)
+    unless self.instance_methods.include?('establish_connection_without_adapter_extensions')
       def establish_connection_with_adapter_extensions(*args)
         establish_connection_without_adapter_extensions(*args)
         ActiveSupport.run_load_hooks(:active_record_connection_established, connection_pool)
       end
-      alias_method :establish_connection, :establish_connection_with_adapter_extensions
+
       alias_method :establish_connection_without_adapter_extensions, :establish_connection
+      alias_method :establish_connection, :establish_connection_with_adapter_extensions
     end
   end
 end
